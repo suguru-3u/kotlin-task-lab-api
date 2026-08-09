20e7a7e81cc9b0cba1ed35229519f3e0be06a44d
suguru-3u <suguru.13579515@gmail.com>
2026-08-10 06:32:32 +0900

    taskを更新するユースケースにドメインサービスを導入

--- task-lab-core/src/main/kotlin/tasklab/core/usecase/task/UpdateTaskInteractor.kt
+++ task-lab-core/src/main/kotlin/tasklab/core/usecase/task/UpdateTaskInteractor.kt
@@ -1,15 +1,28 @@
 package tasklab.core.usecase.task
 
+import com.github.michaelbull.result.Err
+import com.github.michaelbull.result.Ok
+import com.github.michaelbull.result.Result
+import com.github.michaelbull.result.onErr
 import tasklab.core.domain.task.domainService.TaskFoundDomainService
 
 class UpdateTaskInteractor(
     private val taskFoundDomainService: TaskFoundDomainService
 ) : UpdateTaskUseCase {
-    override fun execute(input: UpdateTaskUseCase.Input): UpdateTaskUseCase.Output {
-        return UpdateTaskUseCase.Output(
-            input.task.id.value,
-            input.task.title.value,
-            input.task.description.value,
+    override fun execute(input: UpdateTaskUseCase.Input): Result<UpdateTaskUseCase.Output, FailureUpdateTask> {
+
+        taskFoundDomainService.execute(input.task.id).onErr {
+            return Err(FailureUpdateTask)
+        }
+
+        return Ok(
+            UpdateTaskUseCase.Output(
+                input.task.id.value,
+                input.task.title.value,
+                input.task.description.value,
+            )
         )
     }
+
+    data object FailureUpdateTask
 }
