6f9a52eba694674cb92a831346a4b33a5ffd1d56
suguru-3u <suguru.13579515@gmail.com>
2026-08-18 07:58:32 +0900

    タスク登録のITが成功するように修正

--- task-lab-api/src/integrationTest/kotlin/task_lab/backend/task_lab_api/TaskRegisterTest.kt
+++ task-lab-api/src/integrationTest/kotlin/task_lab/backend/task_lab_api/TaskRegisterTest.kt
@@ -1,38 +1,61 @@
 package task_lab.backend.task_lab_api
 
+import io.kotest.core.extensions.ApplyExtension
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.extensions.spring.SpringExtension
+import io.kotest.matchers.shouldBe
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc
+import org.springframework.context.annotation.Import
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.post
+import tools.jackson.databind.ObjectMapper
+
+// TODO: テストコードの公式サイト、技術選定に関してmdファイルにまとめる
+// https://kotest.io/docs/framework/project-setup.html
 
 @SpringBootTest
+@ApplyExtension(SpringExtension::class)
+@Import(MySqlContainerConfig::class)
+@AutoConfigureMockMvc
 class TaskRegisterTest(
     val jdbcTemplate: JdbcTemplate,
     val mockMvc: MockMvc,
+    val objectMapper: ObjectMapper
 ) : FreeSpec({
 
     "タスクが登録できること" {
+
+        val request = CreateTaskRequest(
+            title = "Test Task",
+            description = "This is a test task."
+        )
         val testDBTitle = "Test Task"
-        val beforeTasks = jdbcTemplate.execute("SELECT * FROM tasks WHERE title")
-        val result = mockMvc.post("/tasks") {
-            accept = MediaType.APPLICATION_JSON
-            param("title", "Test Task")
-            param("description", "This is a test task.")
+
+        val beforeCount = jdbcTemplate.queryForObject(
+            "SELECT COUNT(*) FROM tasks WHERE title = ?",
+            Int::class.java,
+            testDBTitle
+        ) ?: 0
+        val result = mockMvc.post("/api/v1/tasks") {
+            contentType = MediaType.APPLICATION_JSON
+            content = objectMapper.writeValueAsString(request)
         }
+        val afterCount = jdbcTemplate.queryForObject(
+            "SELECT COUNT(*) FROM tasks WHERE title = ?",
+            Int::class.java,
+            testDBTitle
+        ) ?: 0
 
-        result.andExpect {
-            status { isCreated() }
-        }
-
-        val afterTasks = jdbcTemplate.execute("SELECT * FROM tasks")
-
-
+        result.andReturn().response.status shouldBe HttpStatus.CREATED.value()
+        afterCount shouldBe beforeCount + 1
     }
-    // テスト用にDBの中身を確認する
-
-
-    // 実際に登録処理を行う
-    // DBにデータが登録されていることを確認する
-})
+}) {
+    data class CreateTaskRequest(
+        val title: String,
+        val description: String,
+    )
+}
